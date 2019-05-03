22dd322e6dd9781a1e544dcf5146e0dcdd4455fe
takuyaOba <letztbattalion001@gmail.com>
2019-05-03 20:06:08 +0900

    完成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,12 +9,28 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(text: tweet_params[:text], user_id: current_user.id)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
   end
 
   private
     def tweet_params
-      params.permit(:name, :image, :text)
+      params.permit(:text)
     end
 
     def move_to_index
---
+++ db/migrate/20190503103742_remove_image_from_tweets.rb
@@ -0,0 +1,4 @@
+class RemoveImageFromTweets < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
