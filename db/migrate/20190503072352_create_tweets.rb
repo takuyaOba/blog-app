e1f3fbfec098e58c6e069c661214bdd2887c85ce
takuyaOba <letztbattalion001@gmail.com>
2019-05-03 16:33:55 +0900

    おk

--- db/migrate/20190503072352_create_tweets.rb
+++ db/migrate/20190503072352_create_tweets.rb
@@ -2,9 +2,9 @@ class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
       t.string      :name
-      t.text        :text
-      t.text        :image
-      t.timestamps null: true
+        t.text        :text
+        t.text        :image
+        t.timestamps null: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_034046) do
+ActiveRecord::Schema.define(version: 2019_05_03_072352) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
