a00d569a507e50638ef3b26aa842300f4550fc1d
takuyaOba <letztbattalion001@gmail.com>
2019-05-04 11:16:53 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
     
     root  'tweets#index'
-    get "welcome" => "welcome#index"
-    get 'home/index' => "home#index"
-    get   'tweets'      =>  'tweets#index'
     get   'tweets/new'  =>  'tweets#new'
     post  'tweets'      =>  'tweets#create'
     get   'users/:id'   =>  'users#show'
